b68f95d1d73e4d4d40be15f85f7a645b8101a5ee
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-09-25 18:40:10 -0300

    initial work

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# This is based on itzg/minecraft-server
+
+FROM itzg/ubuntu-openjdk-7
+
+MAINTAINER Rafael G. Martins <rafael@rafaelmartins.eng.br>
+
+RUN apt-get install -y wget unzip
+
+EXPOSE 25565
+
+ADD start.sh /start
+
+VOLUME ['/data']
+ADD server.properties /tmp/server.properties
+WORKDIR /data
+
+CMD /start
+
+ENV MOTD A Minecraft (FTBLite2) Server Powered by Docker
+ENV LEVEL world
+ENV JVM_OPTS -Xms2048m -Xmx2048m -XX:PermSize=128m
+ENV VERSION 1.1.9
---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+VERSION="${VERSION//./_}"
+ZIPFILE="FTBLite2.zip"
+
+if [[ "${VERSION}" = "LATEST" ]]; then
+    VERSION="$(wget -q -O - http://www.feed-the-beast.com/server-download | grep -Po "(FTBLite2%5E[\d_]+)" | sed 's/FTBLite2%5E//')"
+fi
+
+cd /data
+
+if [ ! -e "${ZIPFILE}" ]; then
+    echo "Downloading modpacks%5EFTBLite2%5E${VERSION}%5EFTBLite2Server.zip -> ${ZIPFILE} ..."
+    wget -c -O "${ZIPFILE}.part" "http://www.creeperrepo.net/FTB2/modpacks%5EFTBLite2%5E${VERSION}%5EFTBLite2Server.zip"
+    mv "${ZIPFILE}"{.part,}
+fi
+
+unzip "${ZIPFILE}"
+
+if [ ! -e server.properties ]; then
+  cp /tmp/server.properties .
+fi
+
+if [ -n "$MOTD" ]; then
+  sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [ -n "$LEVEL" ]; then
+  sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [ -n "$OPS" ]; then
+  echo $OPS | awk -v RS=, '{print}' >> ops.txt
+fi
+
+java $JVM_OPTS -jar FTBServer-*.jar nogui
